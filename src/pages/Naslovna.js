af0fe37fc5a7d26533142d8640270116605cd55d
kresimircina <kresimircina@gmail.com>
2026-03-03 23:25:36 +0100

    još posla na naslovna.js

--- src/pages/Naslovna.js
+++ src/pages/Naslovna.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import FeaturedImg from "../components/FeaturedImg";
 import HeroSection from "../components/HeroSection";
+import { Link } from "react-router-dom"
 
 //const BASE_URL = process.env.REACT_APP_API_URL
 
@@ -35,7 +36,22 @@ const Naslovna = () => {
       />
       {/*<FeaturedImg page={page} fallback="https://placehold.co/600x400" size="full"  />*/}
       <div dangerouslySetInnerHTML={{ __html:page.content.rendered }} />
+      <div className="row">
+        <div className="col-md-4 map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6048.142158441964!2d19.001158779587325!3d45.226966727294915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475c878313858ab3%3A0x419bbae0cd816d7!2sOroli%C4%8Dka%20ul.%2025%2C%2032242%2C%20Berak!5e1!3m2!1sen!2shr!4v1772559855008!5m2!1sen!2shr"
+           width="600" height="450" style={{ border: 0 }} allowfullscreen="" loading="lazy" 
+           referrerpolicy="no-referrer-when-downgrade" title="mapa lokacije IVi-Catering"></iframe>
+        </div>
+        <div className="col-md-8 d-flex flex-column align-items-center justify-content-center">
+          <div className="text-center mb-2">
+            <Link to="/kontakt" className="btn-contact-main">Kontaktirajte nas</Link>
+          </div>
+        </div>
+        
+      </div>
     </>
+      
+      
   );
 };
 
